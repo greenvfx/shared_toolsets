15b7b03a560279dd1debfe5867115f133ef3197e
greenvfx <latest.green@gmail.com>
2016-05-24 11:21:20 +0300

    Update add_me_to_menu.py

--- add_me_to_menu.py
+++ add_me_to_menu.py
@@ -1,8 +1,8 @@
 # shared_toolsets
 # Setup your SharedToolSets folder here
 if nuke.GUI:
-  import shared_toolsets
-  toolbar = nuke.menu("Nodes")
-  sharedToolSetsPath = "C:/some/path/SharedToolSets" # or "/some/path/SharedToolSets"
-  shared_toolsets.setSharedToolSetsPath(sharedToolSetsPath)
-  shared_toolsets.createToolsetsMenu(toolbar)
+	import shared_toolsets
+	toolbar = nuke.menu("Nodes")
+	sharedToolSetsPath = "C:/some/path/SharedToolSets" # or "/some/path/SharedToolSets"
+	shared_toolsets.setSharedToolSetsPath(sharedToolSetsPath)
+	shared_toolsets.createToolsetsMenu(toolbar)
