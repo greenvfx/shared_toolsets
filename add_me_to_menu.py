b94574369490648f3257eeb455d06b5933c219dc
Vitaly Musatov <latest.green@gmail.com>
2017-04-29 20:44:38 +0300

    Update add_me_to_menu.py

--- add_me_to_menu.py
+++ add_me_to_menu.py
@@ -1,8 +1,37 @@
 # shared_toolsets
 # Setup your SharedToolSets folder here
+
+import sys
+
 if nuke.GUI:
-	import shared_toolsets
-	toolbar = nuke.menu("Nodes")
-	sharedToolSetsPath = "C:/some/path/SharedToolSets" # or "/some/path/SharedToolSets"
-	shared_toolsets.setSharedToolSetsPath(sharedToolSetsPath)
-	shared_toolsets.createToolsetsMenu(toolbar)
+
+  import shared_toolsets
+  
+  sharedToolSetsPaths = {
+    "linux2" : "/mnt/some/nice/place/SharedToolSets",   #LINUX
+    "win32"  : "C:/some/nice/place/SharedToolSets",     #WINDOWS
+    "darwin" : "/Volume/some/nice/place/SharedToolSets" #MACOS
+  }
+
+  def toolSetsFilenameFilter(filename):
+      if nuke.env['MACOS']:
+          # uppercase
+          filename = filename.replace( 'P:', '/Volumes/Project' )
+          # lowercase
+          filename = filename.replace( 'p:', '/Volumes/Project' )
+      elif nuke.env['WIN32']:
+          # lowercase
+          filename = filename.replace( 'D:', 'C:' )
+          filename = filename.replace( '/Volumes/Project', 'P:' )
+      elif nuke.env['LINUX']:
+          filename = filename.replace( 'P:', '/mnt/project' )
+          filename = filename.replace( '/Volumes/Project', '/mnt/project' )
+      return filename
+
+  platform = sys.platform
+
+  sharedToolSetsPath = sharedToolSetsPaths[platform]
+  shared_toolsets.setSharedToolSetsPath(sharedToolSetsPath)
+  shared_toolsets.addFileFilter(toolSetsFilenameFilter)
+
+  shared_toolsets.createToolsetsMenu(toolbar)
